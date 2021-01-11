1e72b13feb445dab2ae24a9b1d06d3ac351ee5b8
Ian K <iankoech9@gmail.com>
2021-01-11 12:51:10 +0300

    Created super user

--- mygallery/urls.py
+++ mygallery/urls.py
@@ -9,8 +9,8 @@ urlpatterns = [
     url(r'^image/(\d+)', views.image, name='image'),
     url(r'^category/(\d+)', views.category, name='category'),
     url(r'^search/$', views.search_results, name='search_results'),
-    url(r'^locations/$', views.image_locations, name='image_locations'),
-    url(r'^copy/(\d+)', views.copy, name='copy')
+    url(r'^locations/$', views.image_location, name='image_location'),
+    url(r'^copy/(\d+)', views.copy_image, name='copy')
 ]
 
 if settings.DEBUG:
